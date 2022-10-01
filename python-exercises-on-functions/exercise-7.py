da8e96602c13385afc2b2c664c9912475fee0a59
unknown <ilemobayojoseph06@gmail.com>
2022-10-02 00:55:28 +0100

    Seventh exercise

---
+++ python-exercises-on-functions/exercise-7.py
@@ -0,0 +1,8 @@
+def uppercase(word):
+    """Define a function that accepts lowercase words 
+    and returns uppercase words."""
+    change = word.upper()
+    print(change)
+
+output = input('Enter a word: ')
+uppercase(output)
